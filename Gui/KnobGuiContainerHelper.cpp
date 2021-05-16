33aa4ddfb476dbe9ca488b9ef03e4ac09df7258e
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2021-05-16 23:44:12 +0200

    WIP

--- Gui/KnobGuiContainerHelper.cpp
+++ Gui/KnobGuiContainerHelper.cpp
@@ -845,6 +845,8 @@ KnobGuiContainerHelper::findKnobGuiOrCreate(const KnobIPtr & knob,
             } else {
                 labelAlignment = Qt::AlignRight;
             }
+            // temp override
+            labelAlignment = Qt::AlignTop;
 
             if (!hasLabel) {
                 layout->addWidget(fieldContainer, rowIndex, 0, 1, 2);
